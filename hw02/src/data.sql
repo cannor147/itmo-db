66ca4f831b705d5ce86acba5de5273c60fcc3bc3
Erofey Bashunov <cannor147@gmail.com>
2021-09-20 14:03:54 +0300

    fix(hw02): updated sql for year2018

--- hw02/src/data.sql
+++ hw02/src/data.sql
@@ -1,14 +1,14 @@
 insert into Groups (group_id, group_name) values
-	(1, 'M3435'),
+	(1, 'M3437'),
 	(2, 'M3436'),
 	(3, 'M3438'),
 	(4, 'M3439');
 
 insert into Students (student_id, student_forename, student_surname, student_sex, group_id) values
 	(1, 'Ерофей', 'Башунов', true, 2),
-	(2, 'Алексей', 'Плешаков', true, 4),
-	(3, 'Галина', 'Григорьева', false, 1),
-	(4, 'Сергей', 'Балахнин', true, 2);
+	(2, 'Дмитрий', 'Гнатюк', true, 4),
+	(3, 'Денис', 'Трибрат', true, 1),
+	(4, 'Юлия', 'Любавина', false, 2);
 	
 insert into Teachers (teacher_id, teacher_forename, teacher_surname, teacher_sex) values
 	(1, 'Георгий', 'Корнеев', true),
@@ -39,14 +39,14 @@ insert into TeacherCourses (course_id, teacher_id, teacher_position) values
 	(8, 3, 3),
 	(9, 4, 3);
 	
-insert into GroupCourses (course_id, group_id) values
-	(6, 1),
-	(6, 2),
-	(6, 3),
-	(6, 4);
+insert into CourseMembers (course_member_id, course_id, student_id, passed) values
+	(1, 6, 1, false),
+	(2, 6, 2, false),
+	(3, 5, 3, true),
+	(4, 5, 4, true);
 	
-insert into Marks (student_id, course_id, points) values
-	(1, 6, 100.00),
-	(2, 6, 100.00),
-	(3, 6, 100.00),
-	(4, 6, 100.00);
+insert into Marks (mark_id, course_member_id, points) values
+	(1, 1, 98.00),
+	(2, 2, 99.00),
+	(3, 3, 96.00),
+	(4, 4, 94.00);
--- hw02/src/scheme.sql
+++ hw02/src/scheme.sql
@@ -22,10 +22,11 @@ create table Courses (
 	semester int not null,
 	is_elective boolean not null
 );
-create table GroupCourses (
-	group_id int not null,
+create table CourseMembers (
+	course_member_id int primary key,
+	student_id int not null,
 	course_id int not null,
-	primary key (group_id, course_id)
+	passed boolean not null
 );
 create table TeacherCourses (
 	teacher_id int not null,
@@ -34,20 +35,18 @@ create table TeacherCourses (
 	primary key (teacher_id, course_id)
 );
 create table Marks (
-	student_id int not null,
-	course_id int not null,
-	points decimal(5, 2) not null,
-	primary key (student_id, course_id)
+	mark_id int primary key,
+	course_member_id int not null,
+	points decimal(5, 2) not null
 );
 
 alter table Marks
 	add constraint ch_marks_points check (points >= 0 AND points <= 100),
-	add foreign key (student_id) references Students (student_id),
-	add foreign key (course_id) references Courses (course_id);
+	add foreign key (course_member_id) references CourseMembers (course_member_id);
 alter table Students
 	add foreign key (group_id) references Groups (group_id);
-alter table GroupCourses
-	add foreign key (group_id) references Groups (group_id),
+alter table CourseMembers
+	add foreign key (student_id) references Students (student_id),
 	add foreign key (course_id) references Courses (course_id);
 alter table TeacherCourses
 	add foreign key (course_id) references Courses (course_id),
