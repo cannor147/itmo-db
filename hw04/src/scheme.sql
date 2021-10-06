6c7912bb1064f7fb38ff9f32107f47f11e527e4f
Erofey Bashunov <cannor147@gmail.com>
2021-10-06 23:29:34 +0300

    fix(hw04): fixed sql bug

--- hw04/src/scheme.sql
+++ hw04/src/scheme.sql
@@ -1,7 +1,7 @@
 create table Groups (
 	groupId int primary key,
 	groupName varchar(10) not null,
-	unique (group_name)
+	unique (groupName)
 );
 create table Students (
 	studentId int primary key,
@@ -29,13 +29,10 @@ create table Marks (
 	primary key (studentId, courseId)
 );
 
-alter table Marks
-	add constraint ch_marks_mark check (mark >= 0 AND mark <= 100),
-	add foreign key (studentId) references Students (studentId),
-	add foreign key (courseId) references Courses (courseId);
-alter table Students
-	add foreign key (groupId) references Groups (groupId);
-alter table Teaching
-	add foreign key (groupId) references Groups (groupId),
-	add foreign key (courseId) references Courses (courseId),
-	add foreign key (lecturerId) references Lecturers (lecturerId);
+alter table Marks		add constraint ch_marks_mark check (mark >= 0 AND mark <= 100);
+alter table Marks		add foreign key (studentId) references Students (studentId);
+alter table Marks		add foreign key (courseId) references Courses (courseId);
+alter table Students	add foreign key (groupId) references Groups (groupId);
+alter table Teaching	add foreign key (groupId) references Groups (groupId);
+alter table Teaching	add foreign key (courseId) references Courses (courseId);
+alter table Teaching	add foreign key (lecturerId) references Lecturers (lecturerId);
